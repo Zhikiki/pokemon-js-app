dd27efcd7a97c4f8dc44545de33ed0258ca13d84
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2022-11-08 02:36:08 +0100

    Added for loop with if-else function
    Loop creates DOM with the help of document-write
    If-else check the weight of pokemon if it is more than 200, it gives additional heading "Wow! It's heavy"

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 let pokemonList = [
   {
-    name: 'Bulbasaur',
+    name: 'bulbasaur',
+    description: `There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.`,
     category: 'Seed',
     abilities: {
       ability_name: 'Overgrow',
@@ -12,9 +13,38 @@ let pokemonList = [
     weaknesses: ['fire', 'psychic', 'flying', 'ice'],
     evolutions: ['Bulbasaur', 'Ivysaur', 'Venusaur'],
   },
+  {
+    name: 'ivysaur',
+    description: `When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.`,
+    category: 'Seed',
+    abilities: {
+      ability_name: 'Overgrow',
+      ability_info: 'Powers up Grass-type moves when the Pokémon’s HP is low.',
+    },
+    heightFt: 3.03,
+    weightLbs: 28.7,
+    types: ['grass', 'poison'],
+    weaknesses: ['fire', 'psychic', 'flying', 'ice'],
+    evolutions: ['Bulbasaur', 'Ivysaur', 'Venusaur'],
+  },
+  {
+    name: 'venusaur',
+    description: `Its plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight.`,
+    category: 'Seed',
+    abilities: {
+      ability_name: 'Overgrow',
+      ability_info: 'Powers up Grass-type moves when the Pokémon’s HP is low.',
+    },
+    heightFt: 6.07,
+    weightLbs: 220.5,
+    types: ['grass', 'poison'],
+    weaknesses: ['fire', 'psychic', 'flying', 'ice'],
+    evolutions: ['Bulbasaur', 'Ivysaur', 'Venusaur'],
+  },
 
   {
-    name: 'Charmander',
+    name: 'charmander',
+    description: `It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.`,
     category: 'Lizard',
     abilities: {
       ability_name: 'Blaze',
@@ -26,9 +56,38 @@ let pokemonList = [
     weaknesses: ['water', 'ground', 'rock'],
     evolutions: ['Charmander', 'Charmeleon', 'Charizard'],
   },
+  {
+    name: 'charmeleon',
+    description: `It has a barbaric nature. In battle, it whips its fiery tail around and slashes away with sharp claws.`,
+    category: 'Flame',
+    abilities: {
+      ability_name: 'Blaze',
+      ability_info: `Powers up Fire-type moves when the Pokémon’s HP is low.`,
+    },
+    heightFt: 3.07,
+    weightLbs: 41.9,
+    types: ['fire'],
+    weaknesses: ['water', 'ground', 'rock'],
+    evolutions: ['Charmander', 'Charmeleon', 'Charizard'],
+  },
+  {
+    name: 'charizard',
+    description: `It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.`,
+    category: 'Flame',
+    abilities: {
+      ability_name: 'Blaze',
+      ability_info: `Powers up Fire-type moves when the Pokémon’s HP is low.`,
+    },
+    heightFt: 5.07,
+    weightLbs: 199.5,
+    types: ['fire', 'flying'],
+    weaknesses: ['water', 'electric', 'rock'],
+    evolutions: ['Charmander', 'Charmeleon', 'Charizard'],
+  },
 
   {
-    name: 'Squirtle',
+    name: 'squirtle',
+    description: `When it retracts its long neck into its shell, it squirts out water with vigorous force.`,
     category: 'Tiny Turtle',
     abilities: {
       ability_name: 'Torrent',
@@ -40,14 +99,81 @@ let pokemonList = [
     weaknesses: ['grass', 'electric'],
     evolutions: ['Squirtle', 'Wartortle', 'Blastoise'],
   },
+  {
+    name: 'wartortle',
+    description: `It is recognized as a symbol of longevity. If its shell has algae on it, that Wartortle is very old.`,
+    category: 'Turtle',
+    abilities: {
+      ability_name: 'Torrent',
+      ability_info: `Powers up Water-type moves when the Pokémon’s HP is low.`,
+    },
+    heightFt: 3.03,
+    weightLbs: 49.6,
+    types: ['water'],
+    weaknesses: ['grass', 'electric'],
+    evolutions: ['Squirtle', 'Wartortle', 'Blastoise'],
+  },
+  {
+    name: 'blastoise',
+    description: `It crushes its foe under its heavy body to cause fainting. In a pinch, it will withdraw inside its shell.`,
+    category: 'Shellfish',
+    abilities: {
+      ability_name: 'Torrent',
+      ability_info: `Powers up Water-type moves when the Pokémon’s HP is low.`,
+    },
+    heightFt: 5.03,
+    weightLbs: 188.5,
+    types: ['water'],
+    weaknesses: ['grass', 'electric'],
+    evolutions: ['Squirtle', 'Wartortle', 'Blastoise'],
+  },
 ];
-console.log(pokemonList);
-console.log(pokemonList[0].name);
 
-let pokemonName = pokemonList[1].name;
-let pokemonHeight = pokemonList[1].heightFt;
-let pokemonWeight = pokemonList[1].weightLbs;
+// This is suppoused HTML structure for DOM
+// <div class="pokemon-list">
+//     <div class="pokemon-list__item">
+//         <img src="./images/bulbasaur.png" alt="Pokemon Bulbasaur image">
+//         <h2>bulbasaur</h2>
+//         <ul>
+//             <li>
+//                 Height: 2.04
+//             </li>
+//             <li>
+//                 Weight: 15.2
+//             </li>
+//         </ul>
+//     </div>
+// </div>
 
-document.write(
-  `Pokemon ${pokemonName} is ${pokemonList[1].category} pokemon. His height is ${pokemonHeight}Ft and his weight is ${pokemonWeight}Lbs. His evolutions are: ${pokemonList[1].evolutions[0]}, ${pokemonList[1].evolutions[1]}, ${pokemonList[1].evolutions[2]}`
-);
+// How can I put loop inside loop, so I can use aarays for types, weeknesses and evolutions
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].weightLbs > 200) {
+    document.write(
+      `<div class="pokemon-list">
+        <div class="pokemon-list__item">
+          <img src="images/${pokemonList[i].name}.png" alt="Pokemon ${pokemonList[i].name} image" />
+          <h2>${pokemonList[i].name}</h2>
+          <h3>WOW! Super heavy!</h3>
+          <ul>
+            <li><span>Height:</span>${pokemonList[i].heightFt}</li>
+            <li><span>Weight:</span>${pokemonList[i].weightLbs}</li>
+          </ul>
+        </div>
+      </div>`
+    );
+  } else {
+    document.write(
+      `<div class="pokemon-list">
+        <div class="pokemon-list__item">
+          <img src="images/${pokemonList[i].name}.png" alt="Pokemon ${pokemonList[i].name} image" />
+          <h2>${pokemonList[i].name}</h2>
+          <ul>
+            <li><span>Height:</span>${pokemonList[i].heightFt}</li>
+            <li><span>Weight:</span>${pokemonList[i].weightLbs}</li>
+          </ul>
+        </div>
+      </div>`
+    );
+  }
+}
+
