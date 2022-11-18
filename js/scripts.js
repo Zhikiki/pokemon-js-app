b4b9f4d603123f4a2010c7d9762636ebc40a45c9
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2022-11-18 12:35:35 +0100

    Worked out Pokemon filter function

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,12 @@ let pokemonRepository = (function () {
   let pokemonList = [
     {
       name: 'bulbasaur',
-      description: `There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.`,
+      description: `There is a plant seed on its back right from 
+      the day this Pokémon is born. The seed slowly grows larger.`,
       category: 'Seed',
       abilities: {
-        ability_name: 'Overgrow',
-        ability_info:
-          'Powers up Grass-type moves when the Pokémon’s HP is low.',
+        abilityName: 'Overgrow',
+        abilityInfo: 'Powers up Grass-type moves when the Pokémon’s HP is low.',
       },
       heightFt: 2.04,
       weightLbs: 15.2,
@@ -20,9 +20,8 @@ let pokemonRepository = (function () {
       description: `When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.`,
       category: 'Seed',
       abilities: {
-        ability_name: 'Overgrow',
-        ability_info:
-          'Powers up Grass-type moves when the Pokémon’s HP is low.',
+        abilityName: 'Overgrow',
+        abilityInfo: 'Powers up Grass-type moves when the Pokémon’s HP is low.',
       },
       heightFt: 3.03,
       weightLbs: 28.7,
@@ -35,9 +34,8 @@ let pokemonRepository = (function () {
       description: `Its plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight.`,
       category: 'Seed',
       abilities: {
-        ability_name: 'Overgrow',
-        ability_info:
-          'Powers up Grass-type moves when the Pokémon’s HP is low.',
+        abilityName: 'Overgrow',
+        abilityInfo: 'Powers up Grass-type moves when the Pokémon’s HP is low.',
       },
       heightFt: 6.07,
       weightLbs: 220.5,
@@ -48,11 +46,12 @@ let pokemonRepository = (function () {
 
     {
       name: 'charmander',
-      description: `It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.`,
+      description: `It has a preference for hot things. 
+      When it rains, steam is said to spout from the tip of its tail.`,
       category: 'Lizard',
       abilities: {
-        ability_name: 'Blaze',
-        ability_info: `Powers up Fire-type moves when the Pokémon’s HP is low.`,
+        abilityName: 'Blaze',
+        abilityInfo: `Powers up Fire-type moves when the Pokémon’s HP is low.`,
       },
       heightFt: 2.0,
       weightLbs: 18.7,
@@ -62,11 +61,12 @@ let pokemonRepository = (function () {
     },
     {
       name: 'charmeleon',
-      description: `It has a barbaric nature. In battle, it whips its fiery tail around and slashes away with sharp claws.`,
+      description: `It has a barbaric nature. 
+      In battle, it whips its fiery tail around and slashes away with sharp claws.`,
       category: 'Flame',
       abilities: {
-        ability_name: 'Blaze',
-        ability_info: `Powers up Fire-type moves when the Pokémon’s HP is low.`,
+        abilityName: 'Blaze',
+        abilityInfo: `Powers up Fire-type moves when the Pokémon’s HP is low.`,
       },
       heightFt: 3.07,
       weightLbs: 41.9,
@@ -79,8 +79,8 @@ let pokemonRepository = (function () {
       description: `It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.`,
       category: 'Flame',
       abilities: {
-        ability_name: 'Blaze',
-        ability_info: `Powers up Fire-type moves when the Pokémon’s HP is low.`,
+        abilityName: 'Blaze',
+        abilityInfo: `Powers up Fire-type moves when the Pokémon’s HP is low.`,
       },
       heightFt: 5.07,
       weightLbs: 199.5,
@@ -94,8 +94,8 @@ let pokemonRepository = (function () {
       description: `When it retracts its long neck into its shell, it squirts out water with vigorous force.`,
       category: 'Tiny Turtle',
       abilities: {
-        ability_name: 'Torrent',
-        ability_info: `Powers up Water-type moves when the Pokémon’s HP is low.`,
+        abilityName: 'Torrent',
+        abilityInfo: `Powers up Water-type moves when the Pokémon’s HP is low.`,
       },
       heightFt: 1.08,
       weightLbs: 19.8,
@@ -105,11 +105,12 @@ let pokemonRepository = (function () {
     },
     {
       name: 'wartortle',
-      description: `It is recognized as a symbol of longevity. If its shell has algae on it, that Wartortle is very old.`,
+      description: `It is recognized as a symbol of longevity. 
+      If its shell has algae on it, that Wartortle is very old.`,
       category: 'Turtle',
       abilities: {
-        ability_name: 'Torrent',
-        ability_info: `Powers up Water-type moves when the Pokémon’s HP is low.`,
+        abilityName: 'Torrent',
+        abilityInfo: `Powers up Water-type moves when the Pokémon’s HP is low.`,
       },
       heightFt: 3.03,
       weightLbs: 49.6,
@@ -119,11 +120,12 @@ let pokemonRepository = (function () {
     },
     {
       name: 'blastoise',
-      description: `It crushes its foe under its heavy body to cause fainting. In a pinch, it will withdraw inside its shell.`,
+      description: `It crushes its foe under its heavy body to cause fainting. 
+      In a pinch, it will withdraw inside its shell.`,
       category: 'Shellfish',
       abilities: {
-        ability_name: 'Torrent',
-        ability_info: `Powers up Water-type moves when the Pokémon’s HP is low.`,
+        abilityName: 'Torrent',
+        abilityInfo: `Powers up Water-type moves when the Pokémon’s HP is low.`,
       },
       heightFt: 5.03,
       weightLbs: 188.5,
@@ -164,17 +166,20 @@ function getPocemonCard(pokemon) {
           </ul>
         </div>`;
   document.write(pokemonItemHtml);
-
-  
 }
 
 pokemonRepository.getAll().forEach(getPocemonCard);
 
 // Filter pokemons by name. I will need to create input field for search
-// Allso i need to write the function that will take attribute from result of function findPokemon and create the card with result of the filter
-function findPokemon(query) {
-  return pokemonRepository.getAll().filter(function (pokemon) {
-    return pokemon.name.indexOf(query) > -1;
+// Allso i need to write the function that will take attribute from
+// result of function findPokemon and create the card with result of the filter
+
+
+function findPokemon(queryValue) {
+  return pokemonRepository.getAll().filter((value) => {
+    if (value.name === queryValue) {
+      console.log(value);
+    }
   });
 }
 console.log(findPokemon('bulbasaur'));
